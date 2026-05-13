ee03bd4ece001a178786aab6c65974c5fc70130e
Nakashima Ryosuke <r.nakashima.a10@panda.kobe-u.ac.jp>
2026-05-13 13:09:50 +0900

    feat: add a function

--- package_common/utils_collocation.py
+++ package_common/utils_collocation.py
@@ -5,7 +5,9 @@ import sys
 
 import numpy as np
 
+from package_common.calc_heinrichs import heinrichs, heinrichs_d, heinrichs_d2
 from package_common.default_logger import DefaultLogger
+from package_common.spectral_deform import ComplexCoordinate
 from package_common.utils_name import create_function_name_logger
 
 
@@ -37,3 +39,51 @@ def calc_collocation_point(i_l: int,
 
     logger.error('Invalid argument')
     sys.exit(1)
+
+
+def spherical_laplacian_heinrichs(
+        m_order: int,
+        n_degree: int,
+        s_pos: float,
+        mu_complex: ComplexCoordinate) -> float | complex:
+    """Calculate the spherical horizontal Laplacian of the Heinrichs
+    basis at a given point.
+
+    Parameters
+    ----------
+    m_order : int
+        The zonal wavenumber (order).
+    n_degree : int
+        The degree of the Heinrichs basis.
+    s_pos : float
+        The position of the point.
+    mu_complex : ComplexCoordinate
+        The complex coordinate for spectral deformation.
+
+    Returns
+    -------
+    float | complex
+        The value of the spherical horizontal Laplacian of the Heinrichs
+        basis at the point.
+    """
+
+    mu: float | complex
+    mu_d: float | complex
+    mu_d2: float | complex
+    if mu_complex.check_spectral_deform():
+        mu = mu_complex.value(s_pos)
+        mu_d = mu_complex.value_d(s_pos)
+        mu_d2 = mu_complex.value_d2(s_pos)
+    else:
+        mu = mu_complex.r_value(s_pos)
+        mu_d = mu_complex.r_value_d(s_pos)
+        mu_d2 = mu_complex.r_value_d2(s_pos)
+
+    sin2: float | complex = 1 - (mu**2)
+
+    return (
+        sin2 * heinrichs_d2(n_degree, s_pos) / (mu_d**2)
+        - (2*mu/mu_d + sin2*mu_d2/(mu_d**3))
+        * heinrichs_d(n_degree, s_pos)
+        - (m_order**2) * heinrichs(n_degree, s_pos) / sin2
+    )
